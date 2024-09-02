7a265d8380fc512bf098c6288be539205af25a07
yurannhassengo <yurannhassengo@gmail.com>
2024-09-02 15:09:37 -0700

    update productList

--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Product } from '../product'
 import {Seed} from '../../data/seed'
 
 export const ProductList = () => {
+
+    const [products,setProducts] = useState([])
+    
 
    const  handleProductUpVote = (productId) => {
 
         console.log(productId +"was upvoted.");
     }
 
+    useEffect(() =>{
+        const seed = Seed.sort((a,b) =>(
+            b.votes - a.votes
+        ));
+
+        setProducts(seed);
+    },[])
+
+
   return (
     <div>
       <div>
         {
-             Seed.map((product) =>(
+             products.map((product) =>(
 
                 <Product 
                     key={'product-' +product.id}
